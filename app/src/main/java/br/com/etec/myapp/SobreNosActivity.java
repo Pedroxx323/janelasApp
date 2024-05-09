38a9b7620c1e2b890f0d0d068f6983d530c59048
Pedroxx323 <pepefreitas2007@gmail.com>
2024-05-09 20:13:28 -0300

    att

--- app/src/main/java/br/com/etec/myapp/SobreNosActivity.java
+++ app/src/main/java/br/com/etec/myapp/SobreNosActivity.java
@@ -1,6 +1,9 @@
 package br.com.etec.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SobreNosActivity extends AppCompatActivity {
+Button btnvoltarmenuprincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class SobreNosActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnvoltarmenuprincipal = findViewById(R.id.btnMenuPrincipalVoltar);
+        btnvoltarmenuprincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Menu_Principal_Activity.class);
+                startActivity();
+                finish();
+            }
         });
     }
 }
